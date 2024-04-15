7c465fac2a9714de15f3ad62a7e14ccbed2eb433
Rob Habraken <roberthabraken@hotmail.com>
2024-04-15 11:05:36 +0200

    Test deployment

--- src/rendering/src/components/Navigation/MainNavigation.tsx
+++ src/rendering/src/components/Navigation/MainNavigation.tsx
@@ -90,7 +90,7 @@ const MainNavigation = (props: MainNavigationProps): JSX.Element => {
             {shopLink}
             <li className="button-menu-item">
               <Link href="/tickets" className="btn-main">
-                {t('Book Tickets') || 'Book Tickets'}
+                {t('Inloggen') || 'Inloggen'}
               </Link>
             </li>
           </ul>
